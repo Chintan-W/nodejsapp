0a2cfcbc48d261710880c23e4adbce8df251dc71
Chintan-W <n01548815@humber.ca>
2023-11-29 11:15:59 -0500

    yeaa

--- config/database2.js
+++ config/database2.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : "mongodb+srv://bchintan99:chintan@cluster0.lbtbsd2.mongodb.net/asn4"
+    url : process.env.DATABASE_url || "mongodb+srv://bchintan99:chintan@cluster0.lbtbsd2.mongodb.net/asn4"
 };
